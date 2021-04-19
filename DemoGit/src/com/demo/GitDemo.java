4f7d5e916dc899b8094a73ab48e862e8be94e5c5
artidd0212 <82807232+artidd0212@users.noreply.github.com>
2021-04-19 16:45:59 +0530

    By arti

--- DemoGit/src/com/demo/GitDemo.java
+++ DemoGit/src/com/demo/GitDemo.java
@@ -2,10 +2,16 @@ package com.demo;
 
 public class GitDemo 
 {
+	public void m1()
+	{ 
+		System.out.println(String[] args)
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("hello git");
 		
 	}
+	
 
 }
